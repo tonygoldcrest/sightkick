e7f55a0cf380959890500bd01dfd77f9c6c5fac4
peancored <peancored@gmail.com>
2026-07-07 20:33:18 +0100

    Refactor support button

--- src/renderer/components/SupportButton/SupportButton.tsx
+++ src/renderer/components/SupportButton/SupportButton.tsx
@@ -1,9 +1,8 @@
-import { Button, Divider } from 'antd';
+import { Button, Divider, Popconfirm } from 'antd';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMugHot, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faMugHot } from '@fortawesome/free-solid-svg-icons';
 import { useApp } from '../../context/AppContext';
-import { IconButton } from '../IconButton';
-import { Tooltip } from '../Tooltip';
+import { popoverStyles } from '../../overlayStyles';
 
 const KOFI_URL = 'https://ko-fi.com/tonygoldcrest';
 
@@ -17,7 +16,7 @@ export function SupportButton() {
   return (
     <>
       <Divider />
-      <div className="flex items-center gap-1">
+      <div className="flex flex-col items-center gap-1">
         <Button
           className="w-full"
           icon={<FontAwesomeIcon icon={faMugHot} />}
@@ -25,12 +24,25 @@ export function SupportButton() {
         >
           Support the project
         </Button>
-        <Tooltip title="Dismiss" placement="left">
-          <IconButton
-            icon={faXmark}
-            onClick={() => setSupportDismissed(true)}
-          />
-        </Tooltip>
+        <Popconfirm
+          title="Hide the support button?"
+          description="You can still support any time from Ko-fi."
+          okText="I already supported"
+          cancelText="Cancel"
+          okType="default"
+          cancelButtonProps={{ type: 'primary' }}
+          placement="top"
+          icon={null}
+          styles={popoverStyles}
+          onConfirm={() => setSupportDismissed(true)}
+        >
+          <button
+            type="button"
+            className="bg-transparent border-0 p-0 cursor-pointer text-xs text-text-dim underline hover:brightness-150"
+          >
+            Already supported
+          </button>
+        </Popconfirm>
       </div>
     </>
   );
